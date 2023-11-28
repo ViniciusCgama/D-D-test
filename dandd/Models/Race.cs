1b6a7216df52f94e68dd01d0a9852040e1f19129
Bruno Donatelli <bruno.donatelli17@gmail.com>
2023-11-28 14:52:01 -0300

    feat(Race): create model

---
+++ dandd/Models/Race.cs
@@ -0,0 +1,14 @@
+﻿namespace dandd.Models;
+
+public class RaceResponse
+{
+    public int Count { get; }
+    public List<Race> Results { get; }
+
+}
+public class Race
+{
+    public string Index { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
